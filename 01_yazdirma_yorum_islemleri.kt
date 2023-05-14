0c5fb0837535164e13b530220c7b344e9a9a45f9
Mustafa Buyukdereli <77589867+companyakis@users.noreply.github.com>
2023-05-15 00:20:30 +0300

    Create 01_yazdirma_yorum_islemleri.kt

---
+++ 01_yazdirma_yorum_islemleri.kt
@@ -0,0 +1,15 @@
+fun main() {
+  println("Günaydın. Benim adım Kotlin.")
+  println("Kotlin güzel bir dildir.")
+  print("print ve println nasıl farklı! Toplama sonucu:  ")
+  print(15 + 15)
+}
+
+/*
+Günaydın. Benim adım Kotlin.
+Kotlin güzel bir dildir.
+print ve println nasıl farklı! Toplama sonucu:  30
+*/
+
+//tek satır yorum da bu şekilde
+//yukarıya dikkat ettiniz mi? çok satırlı yorum /* ... */ ile oldu
